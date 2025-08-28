dd4146add0fd7791c034b2c7c1ed6be683da7108
larry <fpnf97@outlook.com>
2025-08-28 20:29:37 +0800

    日常学习2025.08.28

---
+++ study/chapt4/chapt4_ws/src/demo_python_service/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'demo_python_service'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name+"/resource", ['resource/face_test.png']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='larry',
+    maintainer_email='fpnf97@outlook.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+          'face_detect = demo_python_service.learn_face_detect:main'  
+        ],
+    },
+)
