6c9622dec31ad989f7ad980e12fd1c87e26bb82a
larry <fpnf97@outlook.com>
2025-08-29 23:53:43 +0800

    tbc

---
+++ study/chapt4/chapt4_ws/src/demo_python_service/demo_python_service/face_detect_node.py
@@ -0,0 +1,43 @@
+import rclpy
+from rclpy.node import Node
+from chapt4_interfaces.srv import FaceDetector
+from ament_index_python.packages import get_package_share_directory
+from cv_bridge import CvBridge
+import cv2
+import face_recognition
+import time
+
+class FaceDetectNode(Node):
+    def __init__(self):
+        super().__init__('face_detection_node')
+        self.bridge = CvBridge()
+        self.service = self.create_service(FaceDetector, '/face_detect', self.detect_face_callback)
+        self.default_image_path = get_package_share_directory('demo_python_service') + '/recources/face_test.png'
+        self.upsample_times = 1
+        self.model = "hog"
+        
+    def detect_face_callback(self, request, response):
+        # todo
+        if request.image.data:
+            cv_image = self.bridge.imgmsg_to_cv2(request.image)
+        else:
+            cv_image = cv2.imread(self.default_image_path)
+        start_time = time.time()
+        self.get_logger().info('Starting face detection...')
+        face_locations = face_recognition.face_locations(cv_image, number_of_times_to_upsample=self.upsample_times, model=self.model)
+        end_time = time.time()
+        self.get_logger().info(f'Face detection completed in {end_time - start_time:.2f} seconds.')
+        response.number = len(face_locations)
+        response.usetime = end_time - start_time
+        for top, right, bottom, left in face_locations:
+            response.top.append(top)
+            response.right.append(right)
+            response.bottom.append(bottom)
+            response.left.append(left)
+        return response
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = FaceDetectNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
